7be5523d35171696d6d279e2f3b505a5328f0a17
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-03-16 21:44:19 -0600

    Add button dark/light

---
+++ src/components/ButtonDarkMode.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Image from 'next/image'
+
+export default function ButtonDarkMode () {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const changeTheme = () => {
+    setIsDarkMode(change => !change)
+    if (isDarkMode) {
+      document.documentElement.classList.remove('dark')
+    } else {
+      document.documentElement.classList.add('dark')
+    }
+  }
+
+  return (
+    <>
+      <button className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center' onClick={changeTheme}>
+        {isDarkMode ? <Image src='/sun.svg' alt='sun' width={16} height={16} className='dark:invert dark:brightness-200 dark:contrast-200' /> : <Image src='/moon.svg' alt='moon' width={16} height={16} />}
+      </button>
+    </>
+  )
+}
