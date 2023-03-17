d6b5c2c76c01663c617eafad5fbd6ce7ed6ec77a
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-03-16 21:56:38 -0600

    Connect to DB

---
+++ src/utils/database.js
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize'
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
+})
+
+let initialized = false
+export async function connectToDatabase () {
+  if (!initialized) {
+    try {
+      await sequelize.authenticate()
+      console.log('Conexion exitosa')
+      await sequelize.sync({ force: true }) // Quitar el force despues
+      console.log('Tablas creadas')
+      initialized = true
+    } catch (error) {
+      console.error('Error de conexion a la BD: ', error)
+    }
+  }
+}
