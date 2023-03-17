158f50d8281231f9f9dd31de81986aaf8758f0e5
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-03-16 21:57:28 -0600

    Create models: Actividades, Estudiantes

---
+++ src/models/estudiante.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../utils/database'
+
+export const TablaEstudiante = sequelize.define('actividad', {
+  idActividad: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING(25),
+    allowNull: false,
+    unique: true
+  },
+  categoria: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  },
+  estado: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  }
+}, {
+  timestamps: true,
+  tableName: 'Actividad'
+})
