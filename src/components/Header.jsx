b312cae7807d7c0d56ed0dbd4e41c45daac56c86
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-03-18 13:39:09 -0600

    Add component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import ButtonDarkMode from './ButtonDarkMode'
+
+export default function Header () {
+  return (
+    <header className='h-16 dark:bg-gray-900 flex items-center justify-between border-b-2 dark:border-white border-black'>
+      <div>
+        <Image width={50} height={50} src='https://upload.wikimedia.org/wikipedia/commons/8/85/Instituto_Tecnologico_de_Oaxaca_-_original.svg' alt='Logo ITO' priority />
+      </div>
+      <ul className='ml-6 flex flex-row gap-3'>
+        <li>
+          <Link href='/' className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href='/actividades' className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'>
+            Actividades
+          </Link>
+        </li>
+        <li>
+          <Link href='/crud' className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'>
+            CRUD Actividades
+          </Link>
+        </li>
+      </ul>
+      <div className='ml-auto'>
+        <ButtonDarkMode />
+      </div>
+    </header>
+  )
+}
--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import ButtonDarkMode from '@/components/ButtonDarkMode'
+import Header from '@/components/Header'
 
 export default function PageLayout ({ children, title = 'Actividades Complementarias', descriptionContent = 'Sistema de Actividades Complementarias' }) {
   return (
@@ -12,31 +10,8 @@ export default function PageLayout ({ children, title = 'Actividades Complementa
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <header className='h-16 dark:bg-gray-900 flex items-center justify-between border-b-2 dark:border-white border-black'>
-        <div>
-          <Image width={50} height={50} src='https://upload.wikimedia.org/wikipedia/commons/8/85/Instituto_Tecnologico_de_Oaxaca_-_original.svg' alt='Logo ITO' priority />
-        </div>
-        <ul className='ml-6 flex flex-row gap-3'>
-          <li>
-            <Link href='/' className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href='/actividades' className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'>
-              Actividades
-            </Link>
-          </li>
-          <li>
-            <Link href='/crud' className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'>
-              CRUD Actividades
-            </Link>
-          </li>
-        </ul>
-        <div className='ml-auto'>
-          <ButtonDarkMode />
-        </div>
-      </header>
+
+      <Header />
 
       <main className='dark:bg-gray-900 h-screen w-screen'>
         {children}
