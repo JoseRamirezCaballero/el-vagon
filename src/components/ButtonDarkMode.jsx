d9455ae7d9a25e45d01560feed31331813815acc
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-03-25 18:47:36 -0600

    Modified: ButtonDarkMode with LocalStorage

--- src/components/ButtonDarkMode.jsx
+++ src/components/ButtonDarkMode.jsx
@@ -1,24 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 export default function ButtonDarkMode () {
   const [isDarkMode, setIsDarkMode] = useState(false)
 
-  const changeTheme = () => {
-    setIsDarkMode(change => !change)
-    if (isDarkMode) {
-      document.documentElement.classList.remove('dark')
+  useEffect(() => {
+    const savedIsDarkMode = localStorage.getItem('isDarkMode')
+    if (savedIsDarkMode !== null) {
+      setIsDarkMode(JSON.parse(savedIsDarkMode))
     } else {
-      document.documentElement.classList.add('dark')
+      setIsDarkMode(false)
     }
+  }, [])
+
+  const changeTheme = () => {
+    const newIsDarkMode = !isDarkMode
+    setIsDarkMode(newIsDarkMode)
+    localStorage.setItem('isDarkMode', JSON.stringify(newIsDarkMode))
   }
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [isDarkMode])
 
   return (
     <>
       <button className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-full text-sm p-2.5 inline-flex items-center justify-center shadow-md' onClick={changeTheme}>
         {isDarkMode ? <Image src='/sun.svg' alt='sun' width={16} height={16} className='dark:invert dark:brightness-200 dark:contrast-200' /> : <Image src='/moon.svg' alt='moon' width={16} height={16} />}
       </button>
-
     </>
   )
 }
