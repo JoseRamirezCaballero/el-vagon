fac79ebafc0787400f9f42ac9b03fd02019bc202
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-05-29 23:14:06 -0600

    Add: Headers for activities

--- src/pages/estudiante/categoria/civica.js
+++ src/pages/estudiante/categoria/civica.js
@@ -7,6 +7,9 @@ export default function Civica ({ actividades }) {
   return (
     <StudentProtectedRoute rol={ROLES.ESTUDIANTE}>
       <PageLayout rol={ROLES.ESTUDIANTE}>
+        <div className='flex justify-center mb-2'>
+          <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl'>Actividades <span className='text-transparent bg-clip-text bg-gradient-to-r to-orange-500 from-amber-400'>Cívicas</span></h1>
+        </div>
         <CardActividad actividades={actividades} />
       </PageLayout>
     </StudentProtectedRoute>
