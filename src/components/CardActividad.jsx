684bdb9376fbd99280678be9fcac5914d5fd097b
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-06-03 16:42:01 -0600

    Inscripcion progress bar

--- src/components/CardActividad.jsx
+++ src/components/CardActividad.jsx
@@ -4,7 +4,9 @@ import { Toaster, toast } from 'sonner'
 import ProfileContext from '@/contexts/dataProfile'
 import { axiosAPI } from '@/utils/constants'
 
-export default function CardActividad ({ actividades }) {
+export default function CardActividad ({ actividad, cantidadEstudiantes }) {
+  const porcentaje = Math.round((cantidadEstudiantes / actividad.capacidad_maxima) * 100)
+  const porcentajeString = `${porcentaje}%`
   const profile = useContext(ProfileContext)
   const [showInscripcionModal, setShowInscripcionModal] = useState(false)
   const [idActividad, setIdActividad] = useState()
@@ -12,10 +14,10 @@ export default function CardActividad ({ actividades }) {
 
   const notification = ({ bool, descriptionToast = '' }) => {
     bool
-      ? toast.success('Actividad modificada', {
+      ? toast.success(`Inscripción ${actividad.periodo} exitosa.`, {
         description: descriptionToast
       })
-      : toast.error('Error al modificar Actividad', {
+      : toast.error(`No fue posible inscribirse a: ${actividad.nombre}`, {
         description: descriptionToast
       })
   }
@@ -28,6 +30,12 @@ export default function CardActividad ({ actividades }) {
 
   const inscribirse = async () => {
     try {
+      if (cantidadEstudiantes >= actividad.capacidad_maxima) {
+        notification({ bool: false, descriptionToast: 'Se ha alcanzado la capacidad máxima de estudiantes.' })
+        setShowInscripcionModal(false)
+        return
+      }
+
       const validate = await axiosAPI.post('/inscripciones/validate', {
         idEstudiante: profile.idEstudiante,
         idActividad,
@@ -39,6 +47,7 @@ export default function CardActividad ({ actividades }) {
           idEstudiante: profile.idEstudiante,
           idActividad
         })
+
         if (response) {
           notification({ bool: true, descriptionToast: 'Te has inscrito correctamente' })
         }
@@ -49,76 +58,55 @@ export default function CardActividad ({ actividades }) {
       setShowInscripcionModal(false)
     } catch (error) {
       setShowInscripcionModal(false)
-      notification({ bool: false, descriptionToast: 'Ya estás inscrito en una actividad durante este periodo, por lo que no es posible registrarte en otra actividad adicional en este momento.' })
+      notification({ bool: false, descriptionToast: `Ya estás inscrito en una actividad durante el periodo ${actividad.periodo}, por lo que no es posible registrarte en otra actividad.` })
     }
   }
 
-  // const [porcentaje, setPorcentaje] = useState(0)
-
-  // useEffect(() => {
-  //   const fetchDatos = async () => {
-  //     try {
-  //       const resInscripcion = await axiosAPI.get(`/inscripciones/${idActividad}`)
-  //       const resActividad = await axiosAPI.get(`/actividades/${idActividad}`)
-  //       const total = resActividad.data.capacidad_maxima
-  //       const cantidadEstudiantes = resInscripcion.data.length
-  //       const porcentaje = Math.round((cantidadEstudiantes / total) * 100)
-  //       setPorcentaje(porcentaje)
-  //     } catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
-
-  //   fetchDatos()
-  // }, [idActividad])
-
   return (
-    <div className='grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center'>
-      {actividades.map(actividad => (
-        <div key={actividad.idActividad} className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-          <div className='flex justify-center'>
-            <Toaster position='bottom-right' expand richColors />
-            <Image className='p-8 rounded-t-lg' src='/img/ajedrez.png' alt='product image' width={150} height={150} />
+    <div key={actividad.idActividad} className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <div className='flex justify-center'>
+        <Toaster position='bottom-right' expand richColors />
+        <Image className='p-8 rounded-t-lg' src='/img/ajedrez.png' alt='product image' width={150} height={150} />
+      </div>
+      <div className='px-5 pb-5'>
+        <h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>{actividad.nombre}</h5>
+        <h6 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>{actividad['responsable.nombres']}</h6>
+        <h6 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>{actividad.lugar}</h6>
+        <div className='flex items-center mt-2.5 mb-5'>
+          <div className='w-full bg-gray-200 rounded-full dark:bg-gray-700'>
+            <div style={{ width: porcentajeString }} className='bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full'>
+              {porcentajeString}
+            </div>
           </div>
-          <div className='px-5 pb-5'>
-            <h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>{actividad.nombre}</h5>
-            <h6 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>{actividad['responsable.nombres']}</h6>
-            <h6 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>{actividad.lugar}</h6>
-            <div className='flex items-center mt-2.5 mb-5'>
-              <div className='w-full bg-gray-200 rounded-full dark:bg-gray-700'>
-                <div className='w-[45%] bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full'>45%</div>
-              </div>
-              <span className='bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3'>{actividad.creditos}</span>
-            </div>
-            <div className='flex items-center justify-between'>
-              <span className='text-2xl font-bold text-gray-900 dark:text-white'>{actividad.horario}</span>
-              <button onClick={() => showModalInscripcion(actividad.idActividad, actividad.periodo)} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Inscribirme</button>
-            </div>
-            <div id='popup-modal' tabIndex='-1' className={`fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center ${showInscripcionModal ? '' : 'hidden'}`}>
-              <div className='relative w-full max-w-md max-h-full'>
-                <div className='relative bg-white rounded-lg shadow dark:bg-gray-700'>
-                  <button onClick={() => setShowInscripcionModal(false)} className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white' data-modal-hide='popup-modal'>
-                    <svg aria-hidden='true' className='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clipRule='evenodd' /></svg>
-                    <span className='sr-only'>Close modal</span>
-                  </button>
-                  <div className='p-6 text-center'>
-                    <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-check mx-auto mb-4 text-green-400 w-14 h-14 dark:text-gray-200' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
-                      <circle cx='12' cy='12' r='10' />
-                      <path d='M8 12l3 3l6 -6' />
-                    </svg>
+          <span className='bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3'>{actividad.creditos}C</span>
+        </div>
+        <div className='flex items-center justify-between'>
+          <span className='text-2xl font-bold text-gray-900 dark:text-white'>{actividad.horario}</span>
+          <button onClick={() => showModalInscripcion(actividad.idActividad, actividad.periodo)} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Inscribirme</button>
+        </div>
+        <div id='popup-modal' tabIndex='-1' className={`fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center ${showInscripcionModal ? '' : 'hidden'}`}>
+          <div className='relative w-full max-w-md max-h-full'>
+            <div className='relative bg-white rounded-lg shadow dark:bg-gray-700'>
+              <button onClick={() => setShowInscripcionModal(false)} className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white' data-modal-hide='popup-modal'>
+                <svg aria-hidden='true' className='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clipRule='evenodd' /></svg>
+                <span className='sr-only'>Close modal</span>
+              </button>
+              <div className='p-6 text-center'>
+                <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-check mx-auto mb-4 text-green-400 w-14 h-14 dark:text-gray-200' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+                  <circle cx='12' cy='12' r='10' />
+                  <path d='M8 12l3 3l6 -6' />
+                </svg>
 
-                    <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>{`¿Te gustaría inscribirte en la actividad "${actividad.nombre}"? Solo podrás inscribirte en una única actividad.`}</h3>
-                    <button onClick={() => inscribirse(actividad.idActividad)} data-modal-hide='popup-modal' type='button' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'>
-                      Si, Inscribirme
-                    </button>
-                    <button onClick={() => setShowInscripcionModal(false)} data-modal-hide='popup-modal' type='button' className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'>No, Cancelar</button>
-                  </div>
-                </div>
+                <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>{`¿Te gustaría inscribirte en la actividad "${actividad.nombre}"? Solo podrás inscribirte en una única actividad.`}</h3>
+                <button onClick={() => inscribirse(actividad.idActividad)} data-modal-hide='popup-modal' type='button' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'>
+                  Si, Inscribirme
+                </button>
+                <button onClick={() => setShowInscripcionModal(false)} data-modal-hide='popup-modal' type='button' className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'>No, Cancelar</button>
               </div>
             </div>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   )
 }
--- src/pages/estudiante/categoria/deportiva.js
+++ src/pages/estudiante/categoria/deportiva.js
@@ -1,38 +1,67 @@
+import { useState, useEffect } from 'react'
 import StudentProtectedRoute from '@/components/ProtectedRoute'
 import PageLayout from '@/components/PageLayout'
 import CardActividad from '@/components/CardActividad'
 import { ROLES, axiosAPI } from '@/utils/constants'
 
 export default function Deportiva ({ actividades }) {
+  const [cantidadEstudiantes, setCantidadEstudiantes] = useState([])
+
+  useEffect(() => {
+    const obtenerCantidadEstudiantes = async () => {
+      try {
+        const responses = await Promise.all(
+          actividades.map((actividad) =>
+            axiosAPI.get(`/inscripciones/${actividad.idActividad}`)
+          )
+        )
+        const cantidadEstudiantes = responses.map(
+          (response) => response.data.length
+        )
+        setCantidadEstudiantes(cantidadEstudiantes)
+      } catch (error) {
+        console.error('Error al obtener la cantidad de estudiantes:', error)
+      }
+    }
+
+    obtenerCantidadEstudiantes()
+    const interval = setInterval(obtenerCantidadEstudiantes, 5000)
+    return () => clearInterval(interval)
+  }, [actividades])
+
   return (
     <StudentProtectedRoute rol={ROLES.ESTUDIANTE}>
       <PageLayout rol={ROLES.ESTUDIANTE}>
         <div className='flex justify-center mb-2'>
           <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl'>Actividades <span className='text-transparent bg-clip-text bg-gradient-to-r to-orange-500 from-amber-400'>Deportivas</span></h1>
         </div>
-        {actividades.length > 0
-          ? (
-            <CardActividad actividades={actividades} />
-            )
-          : (
-            <figure className='max-w-screen-md mx-auto text-center mt-5 p-4 lg:p-0'>
-              <p className='text-2xl font-medium text-gray-900 dark:text-white'>Por el momento no hay actividades extraescolares deportivas.</p>
-              <div role='status' className='space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center mt-6'>
-                <div className='flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700'>
-                  <svg className='w-12 h-12 text-gray-200' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' fill='currentColor' viewBox='0 0 640 512'><path d='M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z' /></svg>
+        <div className={actividades.length > 0 ? 'grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center' : ''}>
+          {actividades.length > 0
+            ? (
+                actividades.map((actividad, index) => (
+                  <CardActividad key={actividad.idActividad} actividad={actividad} cantidadEstudiantes={cantidadEstudiantes[index]} />
+                ))
+              )
+            : (
+              <figure className='max-w-screen-md mx-auto text-center mt-5 p-4 lg:p-0'>
+                <p className='text-2xl font-medium text-gray-900 dark:text-white'>Por el momento no hay actividades extraescolares deportivas.</p>
+                <div role='status' className='space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center mt-6'>
+                  <div className='flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700'>
+                    <svg className='w-12 h-12 text-gray-200' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' fill='currentColor' viewBox='0 0 640 512'><path d='M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z' /></svg>
+                  </div>
+                  <div className='w-full'>
+                    <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-48 mb-4' />
+                    <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5' />
+                    <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 mb-2.5' />
+                    <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5' />
+                    <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5' />
+                    <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[360px]' />
+                  </div>
+                  <span className='sr-only'>Loading...</span>
                 </div>
-                <div className='w-full'>
-                  <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-48 mb-4' />
-                  <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5' />
-                  <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 mb-2.5' />
-                  <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5' />
-                  <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5' />
-                  <div className='h-2 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[360px]' />
-                </div>
-                <span className='sr-only'>Loading...</span>
-              </div>
-            </figure>
-            )}
+              </figure>
+              )}
+        </div>
       </PageLayout>
     </StudentProtectedRoute>
   )
